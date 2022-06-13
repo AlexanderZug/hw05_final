81aec02e0c79919bce0f680028a11ea1f1b979f3
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-06-13 20:00:19 +0300

    add config.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from yatube import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
@@ -21,7 +23,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v22#!j*((n3h45^2m8%51o*5g9v1hn+_bi1an^s73$(-i5z^47'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
