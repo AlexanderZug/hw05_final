40fd67eee4d3eb141a513f5791ae6c9c038c09de
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-07-10 19:08:52 +0300

    Update settings.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import uuid
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from yatube import config
 
@@ -23,7 +23,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.SECRET_KEY
+SECRET_KEY = uuid.uuid4()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
