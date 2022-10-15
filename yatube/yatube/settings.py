093414131da702859b5c6448e5640d69737abcdc
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-10-15 10:10:18 +0500

    minor

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -26,7 +26,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 SECRET_KEY = 'not_secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '51.250.111.106',
